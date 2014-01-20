6549568093bd57981986f325a19e243c8cb14bbe
zhangda <zhangda@zhangdas-MacBook-Pro.local>
2014-01-20 17:18:18 +0800

    adjust model params

--- models/category.js
+++ models/category.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose')
 var categorySchema = new mongoose.Schema({
   name:{type:String, required: true},
   key:{type: String, required: true, index:{unique:true} },
+  discripton:{type: String},
   isDisplay:{type:Boolean, default:true},
-  expended:{type:Boolean, default:true},
+  expanded:{type:Boolean, default:true},
   displayOrder:Number,
   apis:[{type:mongoose.Schema.Types.ObjectId, ref:'Api'}]
 })
