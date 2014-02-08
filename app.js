ef884d3cf6b608d4705edcb52388ec1c76ab7c9b
zhangda <zhangda@MacBook-Pro.local>
2014-02-08 17:07:28 +0800

    large file upload

--- app.js
+++ app.js
@@ -23,12 +23,11 @@ db.once('open', function callback () {
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser({ keepExtensions: true, uploadDir: path.join(__dirname, 'tmp' )}));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -64,7 +63,7 @@ app.get('/api/query', api.query)
 app.del('/api/:id', auth, api.remove)
 app.put('/api/:id', auth, api.update)
 
-app.post('/upload', auth, upload.post)
+app.post('/upload', upload.post)
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,10 @@
 var fs = require('fs')
 var path = require('path')
-
+var formidable = require('formidable')
+var util = require('util');
+ 
 exports.post = function(req, res){
-  var upload_path = req.files.file.path
+  /*var upload_path = req.files.file.path
   var download_path = 'public/download/' + req.files.file.originalFilename
   var display_path = 'download/' + req.files.file.originalFilename
  
@@ -12,5 +14,25 @@ exports.post = function(req, res){
        if(err) return res.json(400,{info:{code:'',message:err.err}})
        return res.json(200, {url:display_path})
      })
+  })*/
+  var form = new formidable.IncomingForm();
+  var upload_dir = "public/download/"
+  form.uploadDir = upload_dir;
+  form.on('error', function(err) { 
+    return res.json(400,{code:'',message:err})
+  })
+  form.parse(req, function(err, fields, files) {
+      if(err) return res.json(400,{info:{code:'',message:err}})
+      else{ //console.log(util.inspect({fields: fields, files: files}))
+      return res.json(200,{path:'/download/'+files.file.name})
+    }
+  });
+  form.on('end', function(fields, files){
+     var old_path = this.openedFiles[0].path
+     var file_name = this.openedFiles[0].name
+    fs.rename(old_path,upload_dir+file_name, function(err){
+      if(err) return res.json(400,{info:{code:'',message:err}})
+    })
   })
 }
+
